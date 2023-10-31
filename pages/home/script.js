b8bb4b3496dd80fb76d5513b539306bcd70c3b53
aynurthr <aynurtahirova203@gmail.com>
2023-10-31 17:22:45 +0400

    email verifier added

--- pages/home/script.js
+++ pages/home/script.js
@@ -1,3 +1,4 @@
+//For Timer
 //November 5, 2023, at 00:00:00
 var date = new Date(Date.UTC(2023, 10, 5, 0, 0, 0));
 date.setHours(date.getHours() - 4); //GMT+4
@@ -26,7 +27,7 @@ function updateTimer() {
 
 setInterval(updateTimer, 1000);
 
-//for sidebar
+//For Sidebar
 var sidebar = document.getElementsByClassName("sidebar")[0];
 var burgerBtn = document.getElementById("burger-btn");
 
@@ -38,4 +39,63 @@ document.addEventListener("click", function (e) {
   if (e.target !== sidebar && e.target !== burgerBtn) {
     sidebar.style.right = "-300px";
   }
+});
+
+//For Subscription
+const emailRegex = /^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+//We have 2 same forms in home page:
+const emailForms = document.querySelectorAll(".emailForm");
+const emailInputs = document.querySelectorAll(".emailInput");
+
+emailForms.forEach((emailForm, index) => {
+  emailForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = emailInputs[index].value;
+
+    if (emailInputs[index].value === "") {
+      Toastify({
+        text: "Please enter your email address to subscribe.",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "right",
+        style: {
+          background: "linear-gradient(to right, #FFD700, #FFB90F)",
+        },
+      }).showToast();
+    } else {
+      if (emailRegex.test(email)) {
+        Toastify({
+          text: "Subscription successful! Watch your inbox for updates.",
+          duration: 3000,
+          destination: "https://github.com/apvarun/toastify-js",
+          newWindow: true,
+          close: true,
+          gravity: "top",
+          position: "right",
+          style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+          },
+        }).showToast();
+        emailInputs[index].value = "";
+      } else {
+        Toastify({
+          text: "Email is not valid!",
+          duration: 3000,
+          destination: "https://github.com/apvarun/toastify-js",
+          newWindow: true,
+          close: true,
+          gravity: "top",
+          position: "right",
+          style: {
+            background: "linear-gradient(to right, #FF0000, #FF5733)",
+          },
+        }).showToast();
+      }
+    }
+  });
 });
