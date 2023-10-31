6cabcf341e5d333f47caf2fc4ca2ffdf2b6528d3
aynurthr <aynurtahirova203@gmail.com>
2023-10-31 16:31:26 +0400

    sidebar added

--- pages/home/script.js
+++ pages/home/script.js
@@ -25,3 +25,17 @@ function updateTimer() {
 }
 
 setInterval(updateTimer, 1000);
+
+//for sidebar
+var sidebar = document.getElementsByClassName("sidebar")[0];
+var burgerBtn = document.getElementById("burger-btn");
+
+burgerBtn.addEventListener("click", function () {
+  sidebar.style.right = "0";
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target !== sidebar && e.target !== burgerBtn) {
+    sidebar.style.right = "-300px";
+  }
+});
